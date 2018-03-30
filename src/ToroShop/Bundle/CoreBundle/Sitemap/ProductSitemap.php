cf3c4562867836c0234501a4cbd8b7ef560045e5
phakpoom <im_bonbonn@hotmail.com>
2018-03-30 14:17:10 +0700

    Sitemap improve

---
+++ src/ToroShop/Bundle/CoreBundle/Sitemap/ProductSitemap.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ToroShop\Bundle\CoreBundle\Sitemap;
+
+use Doctrine\Common\Collections\Collection;
+use Presta\SitemapBundle\Service\UrlContainerInterface;
+use Presta\SitemapBundle\Sitemap\Url\GoogleMultilangUrlDecorator;
+use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductTranslationInterface;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\Component\Resource\Model\TranslationInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Toro\SeoBundle\Sitemap\AbstractSitemapListener;
+
+class ProductSitemap extends AbstractSitemapListener
+{
+    const SECTION = 'product';
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @var ChannelContextInterface
+     */
+    private $channelContext;
+
+    /**
+     * @var array
+     */
+    private $channelLocaleCodes;
+
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        ProductRepositoryInterface $repository,
+        ChannelContextInterface $channelContext
+    )
+    {
+        parent::__construct($urlGenerator);
+        $this->repository = $repository;
+        $this->channelContext = $channelContext;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function registerUrls(UrlContainerInterface $urls): void
+    {
+        $products = $this->getProducts();
+
+        /** @var ProductInterface $product */
+        foreach ($products as $product) {
+            $translations = $this->getTranslations($product);
+            $isMultiLang = 2 <= count($translations);
+            $url = new UrlConcrete(
+                $this->urlGenerator->generate(
+                    'sylius_shop_product_show',
+                    [
+                        'slug' => $product->getSlug(),
+                        '_locale' => $product->getTranslation()->getLocale()
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                ),
+                new \Datetime,
+                'always',
+                0.7
+            );
+
+            if (!$isMultiLang) {
+                $urls->addUrl($url, self::SECTION);
+                continue;
+            }
+
+            $url = new GoogleMultilangUrlDecorator($url);
+            /** @var ProductTranslationInterface $translation */
+            foreach ($translations as $translation) {
+                $url->addLink($this->urlGenerator->generate(
+                    'sylius_shop_product_show',
+                    [
+                        'slug' => $translation->getSlug(),
+                        '_locale' => $translation->getLocale()
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                ), $translation->getLocale());
+            }
+
+            $urls->addUrl($url, self::SECTION);
+        }
+    }
+
+    /**
+     * @return array|Collection|ProductInterface[]
+     */
+    private function getProducts()
+    {
+        return $this->repository->createQueryBuilder('o')
+            ->addSelect('translation')
+            ->innerJoin('o.translations', 'translation')
+            ->andWhere(':channel MEMBER OF o.channels')
+            ->andWhere('o.enabled = :enabled')
+            ->setParameter('channel', $this->channelContext->getChannel())
+            ->setParameter('enabled', true)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @return Collection|ProductTranslationInterface[]
+     */
+    private function getTranslations(ProductInterface $product): Collection
+    {
+        return $product->getTranslations()->filter(function (TranslationInterface $translation) {
+            return $this->localeInLocaleCodes($translation);
+        });
+    }
+
+    /**
+     * @param TranslationInterface $translation
+     * @return bool
+     */
+    private function localeInLocaleCodes(TranslationInterface $translation): bool
+    {
+        return in_array($translation->getLocale(), $this->getLocaleCodes());
+    }
+
+    /**
+     * @return array
+     */
+    private function getLocaleCodes(): array
+    {
+        if ($this->channelLocaleCodes === null) {
+            /** @var ChannelInterface $channel */
+            $channel = $this->channelContext->getChannel();
+            $this->channelLocaleCodes = $channel->getLocales()->map(function (LocaleInterface $locale) {
+                return $locale->getCode();
+            })->toArray();
+        }
+
+        return $this->channelLocaleCodes;
+    }
+}
--- src/ToroShop/Bundle/CoreBundle/Sitemap/TaxonSitemap.php
+++ src/ToroShop/Bundle/CoreBundle/Sitemap/TaxonSitemap.php
@@ -4,25 +4,25 @@ declare(strict_types=1);
 
 namespace ToroShop\Bundle\CoreBundle\Sitemap;
 
-use BitBag\SyliusCmsPlugin\Entity\PageInterface;
-use BitBag\SyliusCmsPlugin\Entity\PageTranslationInterface;
-use BitBag\SyliusCmsPlugin\Repository\PageRepositoryInterface;
 use Presta\SitemapBundle\Service\UrlContainerInterface;
 use Presta\SitemapBundle\Sitemap\Url\GoogleMultilangUrlDecorator;
 use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+use Sylius\Component\Taxonomy\Model\TaxonTranslationInterface;
+use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Toro\SeoBundle\Sitemap\AbstractSitemapListener;
+use ToroShop\Bundle\CoreBundle\Model\TaxonInterface;
 
-class CmsPageSitemap extends AbstractSitemapListener
+class TaxonSitemap extends AbstractSitemapListener
 {
-    const SECTION = 'cms_page';
+    const SECTION = 'taxon';
 
     /**
-     * @var PageRepositoryInterface
+     * @var TaxonRepositoryInterface
      */
     private $repository;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator, PageRepositoryInterface $repository)
+    public function __construct(UrlGeneratorInterface $urlGenerator, TaxonRepositoryInterface $repository)
     {
         parent::__construct($urlGenerator);
         $this->repository = $repository;
@@ -33,23 +33,27 @@ class CmsPageSitemap extends AbstractSitemapListener
      */
     public function registerUrls(UrlContainerInterface $urls): void
     {
-        $pages = $this->repository->findBy(['enabled' => true]);
-        /** @var PageInterface $page */
-        foreach ($pages as $page) {
-            $translations = $page->getTranslations();
+        $taxons = $this->repository->findAll();
+        /** @var TaxonInterface $taxon */
+        foreach ($taxons as $taxon) {
+            if ($taxon->isRoot()) {
+                continue;
+            }
+
+            $translations = $taxon->getTranslations();
             $isMultiLang = 2 <= count($translations);
             $url = new UrlConcrete(
                 $this->urlGenerator->generate(
-                    'toro_shop_cms_page_show',
+                    'sylius_shop_product_index',
                     [
-                        'slug' => $page->getSlug(),
-                        '_locale' => $page->getTranslation()->getLocale()
+                        'slug' => $taxon->getSlug(),
+                        '_locale' => $taxon->getTranslation()->getLocale()
                     ],
                     UrlGeneratorInterface::ABSOLUTE_URL
                 ),
-                $page->getUpdatedAt() ?: $page->getCreatedAt(),
-                'weekly',
-                0.8
+                new \Datetime,
+                'always',
+                0.7
             );
 
             if (!$isMultiLang) {
@@ -58,10 +62,10 @@ class CmsPageSitemap extends AbstractSitemapListener
             }
 
             $url = new GoogleMultilangUrlDecorator($url);
-            /** @var PageTranslationInterface $translation */
+            /** @var TaxonTranslationInterface $translation */
             foreach ($translations as $translation) {
                 $url->addLink($this->urlGenerator->generate(
-                    'toro_shop_cms_page_show',
+                    'sylius_shop_product_index',
                     [
                         'slug' => $translation->getSlug(),
                         '_locale' => $translation->getLocale()
